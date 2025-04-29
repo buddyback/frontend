1bb8b2acd3d4d5db63d21ada6cd3da45ac10683e
federico <fraponi98@gmail.com>
2025-04-29 10:45:02 +0200

    add register form

--- components/forms/login-form.tsx
+++ components/forms/login-form.tsx
@@ -8,6 +8,9 @@ import {Label} from "@/components/ui/label"
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card"
 import {useAuth} from "@/providers/auth-provider";
+import {useMutation} from "@tanstack/react-query";
+import {djangoInstance} from "@/config/axios-config";
+import {toast} from "sonner";
 
 export function LoginForm() {
 
@@ -18,6 +21,42 @@ export function LoginForm() {
         e.preventDefault();
         login(loginData.username, loginData.password);
     };
+
+    const emptyRegisterData = {
+        username: "",
+        email: "",
+        password: "",
+        re_password: "",
+    }
+
+    const [registerData, setRegisterData] = useState(emptyRegisterData);
+
+    const registerMutation = useMutation({
+        mutationFn: async () => {
+            const res = await djangoInstance.post("/auth/users/", registerData)
+            return res.data;
+        },
+        onSuccess: async () => {
+            toast.success("User registered successfully.");
+            setRegisterData(emptyRegisterData);
+        },
+        onError: async (error) => {
+            if (error instanceof Error) {
+                toast.error(error.message);
+            } else {
+                toast.error("An error occurred while registering the user.");
+            }
+        }
+    })
+
+    const handleRegisterSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (registerData.password !== registerData.re_password) {
+            toast.error("Passwords do not match.");
+            return;
+        }
+        registerMutation.mutate();
+    }
 
     return (
         <div className={"grid gap-10"}>
@@ -80,32 +119,63 @@ export function LoginForm() {
                             <CardTitle>Create an account</CardTitle>
                             <CardDescription>Enter your information to create an account</CardDescription>
                         </CardHeader>
-                        <form onSubmit={() => {
-                        }}>
+                        <form onSubmit={handleRegisterSubmit}>
                             <CardContent className="space-y-4">
                                 <div className="space-y-2">
-                                    <Label htmlFor="name">Name</Label>
-                                    <Input id="name" placeholder="John Doe" required/>
+                                    <Label htmlFor="username">Username</Label>
+                                    <Input
+                                        id="username"
+                                        placeholder="John Doe"
+                                        required
+                                        onChange={(e) => setRegisterData({...registerData, username: e.target.value})}
+                                        value={registerData.username}
+                                    />
                                 </div>
                                 <div className="space-y-2">
                                     <Label htmlFor="email">Email</Label>
-                                    <Input id="email" type="email" placeholder="name@example.com" required/>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="name@example.com"
+                                        required
+                                        onChange={(e) => setRegisterData({...registerData, email: e.target.value})}
+                                        value={registerData.email}
+                                    />
                                 </div>
                                 <div className="space-y-2">
                                     <Label htmlFor="password">Password</Label>
-                                    <Input id="password" type="password" required/>
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        required
+                                        onChange={(e) => setRegisterData({...registerData, password: e.target.value})}
+                                        value={registerData.password}
+                                    />
                                 </div>
                                 <div className="space-y-2">
                                     <Label htmlFor="confirm">Confirm Password</Label>
-                                    <Input id="confirm" type="password" required/>
+                                    <Input
+                                        id="confirm"
+                                        type="password"
+                                        required
+                                        onChange={(e) => setRegisterData({
+                                            ...registerData,
+                                            re_password: e.target.value
+                                        })}
+                                        value={registerData.re_password}
+                                    />
                                 </div>
                             </CardContent>
                             <CardFooter
                                 className={"mt-10"}
                             >
-                                <Button type="submit" className="w-full bg-teal-600 hover:bg-teal-700"
-                                        disabled={loading}>
-                                    {loading ? "Creating account..." : "Create account"}
+                                <Button
+                                    type="submit"
+                                    className="w-full"
+                                    variant={"accent"}
+                                    disabled={registerMutation.isPending}
+                                >
+                                    {registerMutation.isPending ? "Creating account..." : "Create account"}
                                 </Button>
                             </CardFooter>
                         </form>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        accent: "bg-teal-600 hover:bg-teal-700 text-primary-foreground shadow-xs",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
