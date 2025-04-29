7b7acaafc12e50aa02a1d955b13ed7d900206f4e
federico <fraponi98@gmail.com>
2025-04-29 18:13:11 +0200

    add env url

--- config/axios-config.ts
+++ config/axios-config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "http://127.0.0.1:8000"
+export const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 export const djangoInstance = axios.create({
     baseURL: baseUrl,
