e3e316b967627a95a921740ae41a4936806df35b
federico <fraponi98@gmail.com>
2025-05-05 17:21:52 +0200

    bigger interval

--- components/badges/is-online-badge.tsx
+++ components/badges/is-online-badge.tsx
@@ -31,7 +31,7 @@ const IsOnlineBadge = ({deviceId, setIsOnline}: IsOnlineBadgeProps) => {
     } = useQuery({
         queryKey: isDeviceAliveQueryKey(username, deviceId),
         queryFn: () => getIsDeviceAlive(deviceId),
-        refetchInterval: 3000,
+        refetchInterval: 40 * 1000, // 40 seconds interval
     })
 
     if (isLoadingIsAlive) {
