058296a90180165df2a7995ff1ea5b8adf79ce3e
federico <fraponi98@gmail.com>
2025-05-15 14:21:53 +0200

    add animations to login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import {LoginForm} from "@/components/forms/login-form";
 import BaseContainer from "@/providers/base-container";
+import {Particles} from "@/components/ui/particles";
 
 export default function Home() {
     return (
         <BaseContainer>
+            <div className="w-screen h-screen absolute inset-0 z-0">
+                <Particles
+                    quantity={300}
+                />
+            </div>
             <div
-                className={"min-h-screen flex flex-col items-center justify-center"}
+                className={"min-h-screen flex flex-col items-center justify-center relative"}
             >
                 <div className={"md:w-3/5"}>
                     <LoginForm/>
