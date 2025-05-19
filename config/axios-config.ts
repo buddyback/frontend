ebf281718247342dc4fce50730111a4799caf9e6
federico <fraponi98@gmail.com>
2025-05-19 17:35:46 +0200

    test fix url

--- config/axios-config.ts
+++ config/axios-config.ts
@@ -1,13 +1,10 @@
 import axios from "axios";
 
 // Use NEXT_PUBLIC_ prefix to make it available to the browser
-// export const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
-
-export const baseURL = "https://api.buddyback.srv.mrlc.cc";
-// export const baseURL = "http://127.0.0.1:8000";
+export const baseUrl = process.env.NEXT_PUBLIC_API_URL || "https://api.buddyback.srv.mrlc.cc";
 
 export const djangoInstance = axios.create({
-    baseURL: baseURL,
+    baseURL: baseUrl,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
